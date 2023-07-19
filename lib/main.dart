bb300e5b56925ef28caf3ddf76bd7647c9d84ce5
Matheus Monteiro Alves <matheus.alves@intelie.com.br>
2023-07-19 19:56:27 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const PerguntaApp());
+}
+
+class PerguntaApp extends StatelessWidget {
+  const PerguntaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?',
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Perguntas'),
+        ),
+        body: Column(
+          children: [
+            Text(perguntas[0]),
+            const ElevatedButton(
+              onPressed: null,
+              child: Column(
+                children: [
+                  Text('Resposta 1'),
+                  Text('Resposta 1.1'),
+                  Text('Resposta 1.2'),
+                  Text('Resposta 1.3'),
+                  Text('Resposta 1.4'),
+                  Text('Resposta 1.5'),
+                  Text('Resposta 1.6'),
+                ],
+              ),
+            ),
+            const ElevatedButton(
+              onPressed: null,
+              child: Text('Resposta 2'),
+            ),
+            const ElevatedButton(
+              onPressed: null,
+              child: Text('Resposta 3'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
